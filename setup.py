55b3c9f437bb55dfa158b5182dc30b2546e55b4a
Quinten Steenhuis <qsteenhuis@gmail.com>
2021-08-17 23:05:11 -0400

    Make module-local file imports work

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ def find_package_data(where='.', package='', exclude=standard_exclude, exclude_d
     return out
 
 setup(name='docassemble.PovertyScale',
-      version='2021.1.1',
+      version='2021.1.2',
       description=('A docassemble extension.'),
       long_description='# PovertyScale\r\n\r\nPoverty scale, updated approximately on an annual basis, to use for calculating\r\nincome eligibility in the United States.\r\n\r\nSee example and demo in demo_poverty_scale.yml\r\n\r\nThis package contains a JSON file, poverty.json, which can be referenced directly,\r\nas well as a module poverty.py which exports `poverty_scale_income_qualifies`\r\n\r\n```python\r\ndef poverty_scale_income_qualifies(total_monthly_income:float, household_size:int=1, multiplier:int=1)->Union[bool,None]:\r\n  """\r\n  Given monthly income, household size, and an optional multiplier, return whether an individual\r\n  is at or below the federal poverty level.\r\n  \r\n  Returns None if the poverty level data JSON could not be loaded.\r\n  """\r\n```',
       long_description_content_type='text/markdown',
