14fbb1ee21c5b8f4dcf13138fffcb72a1107ae68
Bryce Willey <bryce.steven.willey@gmail.com>
2022-09-27 20:18:09 -0400

    Run mypy, run unittests on GitHub
    Also ran a few manual tests, changed the Response error string to
    be properly JSON parsed

--- docassemble/PovertyScale/poverty_endpoint.py
+++ docassemble/PovertyScale/poverty_endpoint.py
@@ -12,7 +12,7 @@ def get_poverty_guidelines():
   if results:
     return jsonify(results)
   else:
-    return Response("{'error': 'Unable to load poverty guidelines from disk.'}", status=503, mimetype="application/json")
+    return Response('{"error": "Unable to load poverty guidelines from disk."}', status=503, mimetype="application/json")
 
 @app.route("/poverty_guidelines/household_size/<household_size>", methods=['GET'])
 def get_household_poverty_guideline(household_size:int):
@@ -22,7 +22,7 @@ def get_household_poverty_guideline(household_size:int):
     state = None
   if (request.args) and request.args.get('multiplier'):
     try:      
-      multiplier = float(request.args.get('multiplier'))
+      multiplier = float(request.args.get('multiplier', 1.0))
     except :
       multiplier = 1.0
   else:
@@ -36,23 +36,23 @@ def get_household_poverty_guideline(household_size:int):
   if results:
     return jsonify({'amount': results, 'update_year': update_year})
   else:
-    return Response("{'error': 'Unable to retrieve poverty guidelines.'}", status=503, mimetype="application/json")
+    return Response('{"error": "Unable to retrieve poverty guidelines."}', status=503, mimetype="application/json")
 
 @app.route("/poverty_guidelines/qualifies/household_size/<household_size>", methods=['GET'])
 def get_household_qualifies(household_size:int):
-  if not request.args or not request.args.get('income'):
-    return Response("{'error': 'Income is required'}", 400, mimetype="application/json")
+  if not request.args or not 'income' in request.args:
+    return Response('{"error": "Income is required"}', 400, mimetype="application/json")
   try:
-    income = int(request.args.get('income'))
+    income = int(request.args['income'])
   except ValueError:
-    return Response("{'error': 'Invalid income value. Please provide an integer.'}", 400, mimetype="application/json")
+    return Response('{"error": "Invalid income value. Please provide an integer."}', 400, mimetype="application/json")
   if str(request.args.get('state')).lower() in ['ak','hi']:
     state = str(request.args.get('state')).lower()
   else:
     state = None
-  if request.args.get('multiplier'):
+  if 'multiplier' in request.args:
     try:      
-      multiplier = float(request.args.get('multiplier'))
+      multiplier = float(request.args['multiplier'])
     except :
       multiplier = 1.0
   else:
@@ -66,5 +66,5 @@ def get_household_qualifies(household_size:int):
   if not results is None:
     return jsonify({'qualifies': results, 'update_year': update_year})
   else:
-    return Response("{'error': 'Unable to retrieve poverty guidelines.'}", status=503, mimetype="application/json")  
+    return Response('{"error": "Unable to retrieve poverty guidelines."}', status=503, mimetype="application/json")
   
